3a90d3c18b885bd4b1ef137670e6c2ecd3cb9bd7
Jin Choi <jinchoi@ucdavis.edu>
2020-09-27 15:19:20 -0700

    added Lato font face, made it the default font in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Global, css } from '@emotion/core';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   SignUp
 } from './pages';
-import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <Global
+      styles={css`
+        @font-face {
+          font-family: "Lato";
+          src: url("https://fonts.googleapis.com/css2?family=Lato&display=swap");
+        }
+
+        body {
+          font-family: "Lato", sans-serif;
+        }
+      `} />
       <Switch>
         <Route path='/signup' component={SignUp} />
       </Switch>
